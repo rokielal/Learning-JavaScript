8f32df3baac31626b88ec91ad5a755315015324b
rokielal <manoranjangupta94@gmail.com>
2023-12-17 09:40:43 +0530

    learned about Array

---
+++ Arrays/06.js
@@ -0,0 +1,19 @@
+// Array destructering
+
+const myArray = ["Value2", "Value2"];
+
+// if we want to assign array values to individual variable
+
+// Normal method
+// let myvar1 = myArray[0];
+// let myvar2 = myArray[1];
+
+// console.log(myvar1);
+// console.log(myvar2);
+
+// Object destructering
+
+let [myvar1, myvar2] = myArray;
+
+console.log(myvar1);
+console.log(myvar2);
